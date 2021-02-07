0e548b56071ecc481af971bb66d031a695ac1d03
Rcharitra1 <71350761+Rcharitra1@users.noreply.github.com>
2021-02-07 12:46:05 -0700

    changed store to function

--- client/src/store.js
+++ client/src/store.js
@@ -13,18 +13,35 @@ import rootReducer from './reducers/index.js';
 //     )
 // );
 
-const createStoreWithMiddleware = createStore(
-    rootReducer,
+// const createStoreWithMiddleware = createStore(
+//     rootReducer,
+//     compose(
+//       applyMiddleware(thunk),
+//       window.navigator.userAgent.includes('Chrome') ?
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
+//     ),
+//   );
+
+
+const initialState={};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer,initialState,
     compose(
-      applyMiddleware(thunk),
-      window.navigator.userAgent.includes('Chrome') ?
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
-    ),
-  );
+        applyMiddleware(...middleware),
+        // window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (a)=> a
+        )   
+    );
+
+
+export default store;
 
 
 
   
 
 
-export default createStoreWithMiddleware;
+// export default createStoreWithMiddleware;
